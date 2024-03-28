7136fce3f03c56093e1f5299abdc7fe64a891880
devnickocodes <devnickocodes@gmail.com>
2024-03-28 13:02:23 +0000

    add the category model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+
+    title = models.CharField(max_length=150)
+    subtitle = models.CharField(max_length=200)
+    category_slug = models.SlugField(null=False, blank=False, unique=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+
+        return f'{self.title}'
