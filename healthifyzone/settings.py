672ed893d712acb22be111b36f3955f89d070def
devnickocodes <devnickocodes@gmail.com>
2024-03-28 12:27:23 +0000

    add a main app

--- healthifyzone/settings.py
+++ healthifyzone/settings.py
@@ -18,7 +18,9 @@ if os.path.isfile('env.py'):
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-devnickocodes-healthifyz-x331i3shha.us1.codeanyapp.com',
+]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -26,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-devnickocodes-healthifyz-x331i3shha.us1.codeanyapp.com',
