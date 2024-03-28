66935edf9092c7ed7389883e13aa9204edf680f2
devnickocodes <devnickocodes@gmail.com>
2024-03-28 13:37:49 +0000

    apply migrations for summernote

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Category, Article
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(Article)
+
+@admin.register(Category)
+class CategoryAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'category_slug', 'created_on',)
+    prepopulated_fields = {'category_slug': ('title',)}
+
+
+@admin.register(Article)  
+class ArticleAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'article_slug', 'created_on', 'approved', 'category')
+    list_filter = ('approved', 'created_on')
+    prepopulated_fields = {'article_slug': ('title',)}
+    summernote_fields = ('content',)
