ed14d0575a2edb3d5fcb08144c4788b3f28e3aaf
devnickocodes <devnickocodes@gmail.com>
2024-03-28 11:54:42 +0000

    add a custom user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,23 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    """
+    Defines a custom user model 
+    extending Django's AbstractUser. 
+    """
+
+    STATUS = (
+        (0, 'Beginner'),
+        (1, 'Intermediate'),
+        (2, 'Advanced'),
+    )
+
+    email = models.EmailField(unique=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    bio = models.TextField(max_length=600, default='', blank=True)
+
+    def __str__(self):
+
+        return f"{self.username} | {self.status}"
