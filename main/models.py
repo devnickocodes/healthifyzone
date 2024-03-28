7ee50457fb449adb54e3af3a1ec7a3196f5cdaaf
devnickocodes <devnickocodes@gmail.com>
2024-03-28 13:28:54 +0000

    add the article model and register it in admin.py

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
 class Category(models.Model):
 
     title = models.CharField(max_length=150)
@@ -8,12 +10,23 @@ class Category(models.Model):
     category_slug = models.SlugField(null=False, blank=False, unique=True)
     created_on = models.DateTimeField(auto_now_add=True)
 
-
     class Meta:
+
         verbose_name_plural = "Categories"
         ordering = ["-created_on"]
-
 
     def __str__(self):
 
         return f'{self.title}'
+
+
+class Article(models.Model):
+
+    title = models.CharField(max_length=200, unique=True)
+    subtitle = models.CharField(max_length=200, blank=True)
+    article_slug = models.SlugField(null=False, blank=False, unique=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
