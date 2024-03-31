19f98fda75901a1b357c847bf72ad43c091ce193
devnickocodes <devnickocodes@gmail.com>
2024-03-31 14:26:13 +0000

    add a template for a single article

--- main/urls.py
+++ main/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.DisplayArticles.as_view(), name='homepage'),
-    path('<slug:slug>/', views.view_article, name='view_article'),
+    path('<slug:article_slug>/', views.view_article, name='view_article'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 # pylint: disable=no-member
 # pylint: disable=missing-docstring
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Article
 # Create your views here.
@@ -10,3 +10,15 @@ class DisplayArticles(generic.ListView):
     queryset = Article.objects.filter(approved=True).order_by("-created_on")
     template_name = 'main/index.html'
     paginate_by = 6 
+
+
+def view_article(request, article_slug):
+
+    queryset = Article.objects.filter(approved=True)
+    article = get_object_or_404(queryset, article_slug=article_slug)
+
+    return render(
+        request,
+        "main/view_article.html",
+        {"article": article},
+    )
