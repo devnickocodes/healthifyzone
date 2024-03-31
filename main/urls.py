df5cdb8e88348342ca1c28d29a187af559806949
devnickocodes <devnickocodes@gmail.com>
2024-03-31 13:31:50 +0000

    ad url mapping for a single article display

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.DisplayArticles.as_view(), name='homepage')
+    path('', views.DisplayArticles.as_view(), name='homepage'),
+    path('<slug:slug>/', views.view_article, name='view_article'),
 ]
