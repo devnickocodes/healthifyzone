6bbeff293056277114d70a4fd562121162e1695e
devnickocodes <devnickocodes@gmail.com>
2024-04-01 08:18:39 +0000

    style the active class

--- healthifyzone/settings.py
+++ healthifyzone/settings.py
@@ -29,7 +29,7 @@ CSRF_TRUSTED_ORIGINS = [
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-devnickocodes-healthifyz-x331i3shha.us1.codeanyapp.com',
