be6731d58ddcde53884d498b136f12a9bd0a918a
devnickocodes <devnickocodes@gmail.com>
2024-04-06 10:09:46 +0000

    add validaiton for form data

--- users/tests.py
+++ users/tests.py
@@ -27,3 +27,15 @@ class TestUserRegistrationForm(TestCase):
         form = UserRegistrationForm(data={'username': 'a' * 17})
         self.assertFalse(form.is_valid())
         self.assertIn('username', form.errors)
+    
+    def test_valid_data(self):
+        form_data = {
+            'first_name': 'first name',
+            'last_name': 'last name',
+            'username': 'username',
+            'email': 'username@example.com',
+            'password1': 'password123@31',
+            'password2': 'password123@31'
+        }
+        form = UserRegistrationForm(data=form_data)
+        self.assertTrue(form.is_valid())
