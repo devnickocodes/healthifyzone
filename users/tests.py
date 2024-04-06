59ec0a967d33ae5c74779b1d3586af458aa31c1a
devnickocodes <devnickocodes@gmail.com>
2024-04-06 09:43:19 +0000

    write test for form initialization

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .forms import UserRegistrationForm
 
 # Create your tests here.
+
+class TestUserRegistrationForm(TestCase):
+
+    def test_form_initialization(self):
+        form = UserRegistrationForm()
+        self.assertIsNotNone(form)
+
