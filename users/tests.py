bef6a14af5ab02eb5365a6e14c8a2a8821defc18
devnickocodes <devnickocodes@gmail.com>
2024-04-06 09:49:46 +0000

    write tests for form fields presence

--- users/tests.py
+++ users/tests.py
@@ -9,3 +9,11 @@ class TestUserRegistrationForm(TestCase):
         form = UserRegistrationForm()
         self.assertIsNotNone(form)
 
+    def test_form_fields_presence(self):
+        form = UserRegistrationForm()
+        self.assertTrue('first_name' in form.fields)
+        self.assertTrue('last_name' in form.fields)
+        self.assertTrue('username' in form.fields)
+        self.assertTrue('email' in form.fields)
+        self.assertTrue('password1' in form.fields)
+        self.assertTrue('password2' in form.fields)
