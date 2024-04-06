8bf2e8ce8787abe5c722fcb25fdab8507aaee641
devnickocodes <devnickocodes@gmail.com>
2024-04-06 09:55:50 +0000

    add max length validator

--- users/forms.py
+++ users/forms.py
@@ -1,10 +1,11 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.core.validators import MaxLengthValidator
 
 class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(help_text='A valid email address, please.', required=True)
-    username = forms.CharField()
+    username = forms.CharField(validators=[MaxLengthValidator(16)])
 
     class Meta:
         model = get_user_model()
