fa2b4d38dc37a8a4da2257b15182904e2411143e
devnickocodes <devnickocodes@gmail.com>
2024-04-13 13:49:53 +0000

    style comments section and adjjust bg color of footer

--- healthifyzone/settings.py
+++ healthifyzone/settings.py
@@ -31,7 +31,7 @@ CSRF_TRUSTED_ORIGINS = [
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEBUG' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-devnickocodes-healthifyz-x331i3shha.us1.codeanyapp.com',
