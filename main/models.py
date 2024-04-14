043b0e63b652069d8905066cc5227f54ffa5e7b5
devnickocodes <devnickocodes@gmail.com>
2024-04-14 08:31:08 +0000

    move comment form inside a collapsible component

--- main/models.py
+++ main/models.py
@@ -54,4 +54,4 @@ class Comment(models.Model):
         ordering = ["-created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} | Article: {self.article}"
+        return f"Comment: {self.body} | Author: {self.comment_author}"
