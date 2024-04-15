102ea295c48b8153fadd8a304081ae5886fc3c0f
devnickocodes <devnickocodes@gmail.com>
2024-04-15 09:25:44 +0000

    style the profile page

--- users/test_views.py
+++ users/test_views.py
@@ -2,7 +2,6 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.shortcuts import reverse
 from django.forms.models import model_to_dict
-from .views import view_profile
 from .forms import userUpdateProfileForm
 
 class ProfileViewTestCase(TestCase):
