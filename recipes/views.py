fafa8281a0675db9019438b6e74ef16c6f652c9b
devnickocodes <devnickocodes@gmail.com>
2024-04-24 09:36:53 +0000

    add function and test to see if it returns the correct user query

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+import os
+import requests
 # Create your views here.
-
 
 def recipe_search(request):
     return render(request, 'recipes/recipe_search.html')
+
+def recipe_search_results(request):
+    if request.method == 'GET':
+        query = request.GET.get('query')
+        return render(request, 'recipes/recipes_search_results.html', context={'query':query})
