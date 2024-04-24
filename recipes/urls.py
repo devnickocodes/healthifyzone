821ee481712f4ed9a34850aa9403aa7b0fecf098
devnickocodes <devnickocodes@gmail.com>
2024-04-24 09:27:43 +0000

    add page for displaying results for recipes, add url mapping

--- recipes/urls.py
+++ recipes/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('search/', views.recipe_search, name='recipe_search'),
+    path('search/results/', views.recipe_search_results, name='recipe_search_results'),
 ]
