af81c528a3a5abc471ba5bb8a10c5d81cde5a17f
devnickocodes <devnickocodes@gmail.com>
2024-04-24 09:44:00 +0000

    make sure the recipe_search_results returns a status code of 200

--- recipes/views.py
+++ recipes/views.py
@@ -3,10 +3,21 @@ import os
 import requests
 # Create your views here.
 
+SPOONACULAR_API_KEY = os.environ.get('SPOONACULAR_API_KEY')
+
 def recipe_search(request):
     return render(request, 'recipes/recipe_search.html')
 
 def recipe_search_results(request):
     if request.method == 'GET':
         query = request.GET.get('query')
-        return render(request, 'recipes/recipes_search_results.html', context={'query':query})
+        if query:
+            endpoint = 'https://api.spoonacular.com/recipes/complexSearch'
+            params = {
+                'query': query,
+                'number': 10,
+                'apiKey': SPOONACULAR_API_KEY
+            }
+            
+            response = requests.get(endpoint, params=params)
+            return render(request, 'recipes/recipes_search_results.html', context={'response': response})
