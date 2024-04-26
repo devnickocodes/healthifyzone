16c333f02d2324ab09c557a4a75426a11076f66b
devnickocodes <devnickocodes@gmail.com>
2024-04-26 09:33:17 +0000

    adjust ajax calls and template to accomodate the two calls, adjust styling of like buttons

--- static/js/ajax_article_likes.js
+++ static/js/ajax_article_likes.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-    $(document).on('click', '.comment-likes', function (e) {
+    $(document).on('click', '.article-likes', function (e) {
         e.preventDefault();
         var $button = $(this);
-        var commentId = $button.data('commentid');
+        var articleId = $button.data('articleid');
         var likesCount = parseInt($button.data('likescount'));
         var likeUrl = $button.data('like-url');
 
@@ -10,17 +10,17 @@ $(document).ready(function() {
             type: 'POST',
             url: likeUrl,
             data: {
-                commentid: commentId,
+                articleid: articleId,
                 csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val(),
                 action: 'post'
             },
             success: function (json) {
-                $button.find('.comment-likes-count').text(json['result']);
+                $button.find('.article-likes-count').text(json['result']);
                 $button.data('likescount', json['result']);
                 $button.find('.fa-heart').toggleClass('liked', json['result'] > likesCount);
             },
             error: function (xhr, errmsg, err) {
-                alert('Failed to like the comment. Please try again later.');
+                alert('Failed to like the article. Please try again later.');
             }
         });
     });
