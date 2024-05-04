8b127e67f0d7b2386d3c95bd1d3bb9a2e1ce09b9
devnickocodes <devnickocodes@gmail.com>
2024-05-04 11:31:51 +0000

    refactor styling classes for links

--- healthifyzone/settings.py
+++ healthifyzone/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 CSRF_TRUSTED_ORIGINS = [
-    '*.codeanyapp.com',
+    'https://*.codeanyapp.com',
 ]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
