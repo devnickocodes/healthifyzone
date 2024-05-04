6ce3e6821bcb0f9f154cb99c74419a769b68d8e3
devnickocodes <devnickocodes@gmail.com>
2024-05-04 11:20:14 +0000

    adjust styling for pagereset password page base_message.txt for the reset password email

--- healthifyzone/settings.py
+++ healthifyzone/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-devnickocodes-healthifyz-x331i3shha.us1.codeanyapp.com',
+    '*.codeanyapp.com',
 ]
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
