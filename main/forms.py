ec10700ff71a4d9216af91377e268760a8c5519a
devnickocodes <devnickocodes@gmail.com>
2024-05-15 08:51:49 +0000

    add create_article.html and create an article form

--- main/forms.py
+++ main/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Comment
+from .models import Article, Comment
 
 
 class CommentForm(forms.ModelForm):
@@ -7,3 +7,8 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
+class ArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ['title', 'subtitle', 'content', 'category', 'featured_article_image']
